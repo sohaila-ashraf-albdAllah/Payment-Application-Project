4af52bda5c3f88757fff158057d69f088d2fbaad
sohaila <99507827+sohaila-ashraf-albdAllah@users.noreply.github.com>
2024-04-08 04:15:45 +0200

    app.c  implementation

--- Application/app.c
+++ Application/app.c
@@ -5,25 +5,116 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+#define MAX_AMOUNT 10000
+
 void appStart()
 {
 	ST_cardData_t cardData;
 	ST_terminalData_t terminalData;
 	ST_transaction_t transactionData;
 	EN_terminalError_t terminalReturn;
-	EN_serverError_t serverReturn;
+	EN_transState_t serverReturn;
 	EN_cardError_t cardReturn;
-	bool exitFlag = true;
 
-
-    printf("Welcome to our Payment Application\n");
-    printf("-----------------------------------------------------------------------------\n");
-	while (exitFlag != false)
+    printf("--------------------- Welcome to our Payment Application ---------------------\n");
+    printf("==============================================================================\n");
+	while (true)
 	{
 		int userChoice = -1;
 		printf("Enter 0 to Exit and 1 to enter your data\n");
-		printf("-----------------------------------------------------------------------------\n");
-		
-		printf("Thanks to use our Payment application\n");
+		while (scanf_s("%d", &userChoice) != 1) 
+		{
+			printf("Invalid input. Please enter an integer (0 OR 1)\n");
+			printf("==============================================================================\n");
+			while (getchar() != '\n') {}
+		}
+
+		switch (userChoice)
+		{
+		case 1:
+			printf("==============================================================================\n");
+			//Card part
+			cardReturn = getCardHolderName(&cardData);
+			while (cardReturn == WRONG_NAME)
+			{
+				printf("Please enter valid Name :\n");
+				cardReturn = getCardHolderName(&cardData);
+			}
+			cardReturn = getCardExpiryDate(&cardData);
+			while (cardReturn == WRONG_EXP_DATE)
+			{
+				printf("Please enter valid EXP Date :\n");
+				cardReturn = getCardExpiryDate(&cardData);
+			}
+			cardReturn = getCardPAN(&cardData);
+			while (cardReturn == WRONG_PAN)
+			{
+				printf("Please enter valid PAN Number :\n");
+				cardReturn = getCardPAN(&cardData);
+			}
+
+			//Terminal part
+			terminalReturn = getTransactionDate(&terminalData);
+			while (terminalReturn == WRONG_DATE)
+			{
+				printf("Please enter valid transaction Date :\n");
+				terminalReturn = getTransactionDate(&terminalData);//** cardData
+			}
+			terminalReturn = isCardExpired(&cardData, &terminalData);
+			if (terminalReturn == EXPIRED_CARD)
+			{
+				printf("Your Card Expired !\n");
+				return;
+			}
+			terminalReturn = setMaxAmount(&terminalData, MAX_AMOUNT);
+			while (terminalReturn == INVALID_AMOUNT)
+			{
+				printf("Please enter valid Amount ( MAX_AMOUNT = 10000 ) :\n");
+				terminalReturn = setMaxAmount(&terminalData, MAX_AMOUNT);
+			}
+			terminalReturn = getTransactionAmount(&terminalData);
+			while (terminalReturn == INVALID_AMOUNT)
+			{
+				printf("Please enter valid Amount ( MAX_AMOUNT = 10000 ) :\n");
+				terminalReturn = getTransactionAmount(&terminalData);
+			}
+			terminalReturn = isBelowMaxAmount(&terminalData);
+			if (terminalReturn == EXCEED_MAX_AMOUNT)
+			{
+				printf("INVALID exceed maximum amount ( MAX_AMOUNT = 10000 )\n");
+				return;
+			}
+
+			// Saving data
+			transactionData.cardHolderData = cardData;
+			transactionData.terminalData = terminalData;
+
+			// Server Part
+			serverReturn = recieveTransactionData(&transactionData);
+			if (serverReturn == DECLINED_STOLEN_CARD)
+			{
+				printf("STOLEN CARD!! \n");
+			}
+			else if (serverReturn == DECLINED_INSUFFECIENT_FUND)
+			{
+				printf("INSUFFECIENT FUND.\n");
+			}
+			else if (serverReturn == APPROVED)
+			{
+				printf("Transaction approved.\n");
+				//printf("Updating Data Base Accounts ...\n");
+				//updateDataBaseAcounts();
+				printf("Saving transaction ...\n");
+			}
+			break;
+		case 0:
+			printf("==============================================================================\n");
+			printf("Thanks to use our Payment application\n");
+			return;
+		default:
+			printf("==============================================================================\n");
+			printf("Please enter a valid choice (0 OR 1)\n");
+			break;
+		}
 	}
 }
